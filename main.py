341c995204b4f7e8fc740493e105b32d0bf5edd0
zZz <57362750+zzzzzzzZzzzzzzZ-pong@users.noreply.github.com>
2022-10-24 18:54:39 +0100

    a

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+my_money_machine = MoneyMachine()
+my_coffee_maker = CoffeeMaker()
+the_menu = Menu()
+
+print(f'Here are all the stuff we got: {the_menu.get_items()}')
