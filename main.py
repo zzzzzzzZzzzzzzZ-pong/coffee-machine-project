ca0bb59b6c42fc78cb04af09c10511d515e576ed
zZz <57362750+zzzzzzzZzzzzzzZ-pong@users.noreply.github.com>
2022-10-24 19:28:02 +0100

    Used OOP to simplify code.

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ my_money_machine = MoneyMachine()
 my_coffee_maker = CoffeeMaker()
 the_menu = Menu()
 
-print(f'Here are all the stuff we got: {the_menu.get_items()}')
+while True:
+    prompt = input(f'​What would you like? ({the_menu.get_items()}): ')
+    if prompt in [':q', 'quit', 'exit', 'stop']:
+        break
+    elif prompt == 'report':
+        my_coffee_maker.report()
+        my_money_machine.report()
+    else:
+        drink = the_menu.find_drink(prompt)    
+        if my_coffee_maker.is_resource_sufficient(drink) and my_money_machine.make_payment(drink.cost):
+            my_coffee_maker.make_coffee(drink)
